9a6962a7cd125f1c2c1e69fdeb22c4ac1dd3a03a
musorkan <45989267+musorkan@users.noreply.github.com>
2021-03-10 22:51:54 -0800

    Delete src/test/java directory

--- src/test/java/FeatureAcode2.java
+++
@@ -1,2 +0,0 @@
-public class FeatureAcode2 {
-}
--- src/test/java/HalloweenTest.java
+++
@@ -1,8 +0,0 @@
-public class HalloweenTest {
-
-    // I am about to give everyone candy for halloween
-}
-//hehehe
-//memem
-//There is not gonna be any school this year!
-//another push
